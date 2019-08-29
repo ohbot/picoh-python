67f3590fbc81a6e1f1a56152c7b6241c86bf60c5
Ohbot <ohbotrobot@gmail.com>
2019-08-29 20:17:23 +0100

    Update setup.py
    made platform specific

--- setup.py
+++ setup.py
@@ -1,19 +1,65 @@
 from distutils.core import setup
 
-setup(
-    name = 'picoh',
-    packages = ['picoh'],
-    package_data={'': ['picohdefinitions.omd','Silence1.wav','PicohSpeech.csv','ohbot.obe']},
-    include_package_data=True,
-    version = '0.122',
-    description = 'Python library for controlling Picoh Robot',
-    author = 'ohbot',
-    author_email = 'info@ohbot.co.uk',
-    url = 'https://github.com/ohbot/picoh',
-    download_url = 'https://github.com/ohbot/ohbotMac-python/archive/1.15.tar.gz',
-    keywords = ['ohbot', 'robot','picoh'],
-    classifiers = [],
-    install_requires=[
-          'pyserial','lxml','playsound','pyobjc','comtypes',
-      ],
-)
+import system
+
+if platform.system() == "Darwin":
+
+    setup(
+        name = 'picoh',
+        packages = ['picoh'],
+        package_data={'': ['picohdefinitions.omd','Silence1.wav','PicohSpeech.csv','ohbot.obe']},
+        include_package_data=True,
+        version = '0.122',
+        description = 'Python library for controlling Picoh Robot',
+        author = 'ohbot',
+        author_email = 'info@ohbot.co.uk',
+        url = 'https://github.com/ohbot/picoh',
+        download_url = 'https://github.com/ohbot/ohbotMac-python/archive/1.15.tar.gz',
+        keywords = ['ohbot', 'robot','picoh'],
+        classifiers = [],
+        install_requires=[
+              'pyserial','lxml','playsound','pyobjc','comtypes',
+          ],
+    )
+if platform.system() == "Linux":
+
+    setup(
+          name = 'picoh',
+          packages = ['picoh'],
+          package_data={'': ['picohdefinitions.omd','Silence1.wav','PicohSpeech.csv','ohbot.obe']},
+          include_package_data=True,
+          version = '0.122',
+          description = 'Python library for controlling Picoh Robot',
+          author = 'ohbot',
+          author_email = 'info@ohbot.co.uk',
+          url = 'https://github.com/ohbot/picoh',
+          download_url = 'https://github.com/ohbot/ohbotMac-python/archive/1.15.tar.gz',
+          keywords = ['ohbot', 'robot','picoh'],
+          classifiers = [],
+          install_requires=[
+                            'pyserial','lxml','playsound','comtypes',
+                            ],
+          )
+
+
+if platform.system() == "Windows":
+    
+    setup(
+          name = 'picoh',
+          packages = ['picoh'],
+          package_data={'': ['picohdefinitions.omd','Silence1.wav','PicohSpeech.csv','ohbot.obe']},
+          include_package_data=True,
+          version = '0.122',
+          description = 'Python library for controlling Picoh Robot',
+          author = 'ohbot',
+          author_email = 'info@ohbot.co.uk',
+          url = 'https://github.com/ohbot/picoh',
+          download_url = 'https://github.com/ohbot/ohbotMac-python/archive/1.15.tar.gz',
+          keywords = ['ohbot', 'robot','picoh'],
+          classifiers = [],
+          install_requires=[
+                            'pyserial','lxml','playsound','comtypes',
+                            ],
+          )
+
+
