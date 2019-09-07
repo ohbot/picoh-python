d0f437da9c58cb22709a01f4dcc72850dd2ca5f7
Ohbot <ohbotrobot@gmail.com>
2019-09-07 18:40:52 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from distutils.core import setup
 import platform
 
 if platform.system() == "Darwin":
-    includes =['pyserial','lxml','playsound','pyobjc','numpy','comtypes',]
+    includes =['pyserial','lxml','playsound','pyobjc','numpy','comtypes']
 
 if platform.system() == "Windows":
-    includes =['pyserial','lxml','playsound''numpy','comtypes',]
+    includes =['pyserial','lxml','playsound','numpy','comtypes']
 
 if platform.system() == "Linux":
     includes =['pyserial','playsound','numpy']
@@ -16,7 +16,7 @@ setup(
     packages = ['picoh'],
       package_data={'': ['picohdefinitions.omd','Silence1.wav','picohspeech.wav','PicohSpeech.csv','ohbot.obe','phonemes.txt','Images/movedown.gif','Images/moveright.gif','Images/off.gif','Images/offsmaller.gif','Images/on.gif','Images/onsmaller.gif','Images/picohlogo.gif','Images/picohlogoOn.gif','Images/picohlogoSmall.gif','Images/plus.gif','Images/resetIcon.gif','Images/savebutton.gif']},
     include_package_data=True,
-    version = '1.0209',
+    version = '1.0210',
     description = 'Python library for controlling Picoh Robot',
     author = 'ohbot',
     author_email = 'info@ohbot.co.uk',
